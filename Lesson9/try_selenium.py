fcdf3493013fe7823ac92b9beaf2e81bcea08bb6
Mayfryn <mayfryn@gmail.com>
2022-08-04 23:30:22 +0300

    upd from mac

--- Lesson9/try_selenium.py
+++ Lesson9/try_selenium.py
@@ -1,8 +1,15 @@
 import time
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 driver = webdriver.Chrome()
+actions = ActionChains(driver)
+
+wait = WebDriverWait()
 driver.get('https://google.com/')
 
 # els = driver.find_element(By.CLASS_NAME, "gLFyf")
@@ -19,10 +26,13 @@ els[0].send_keys("python")
 
 time.sleep(3)
 
-button = driver.find_elements(By.CLASS_NAME, "gNO89b")
+# button = driver.find_elements(By.CLASS_NAME, "gNO89b")
+button = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "gNO89b")), message="Button Search is not clickable")
 button[0].click()
 
 els = driver.find_elements(By.PARTIAL_LINK_TEXT, "Python")
 print(len(els))
+
+actions.context_click()
 
 # driver.quit()
